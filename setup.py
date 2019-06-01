49c0488cbf46a7baa536e28caf38e1d4da7c115a
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2019-06-01 14:50:51 -0500

    remote torch reference in setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,4 @@
-import torch
 from setuptools import setup, find_packages
-
-if not torch.cuda.is_available():
-    print("Warning: Torch did not find available GPUs on this system.\n",
-          "If your intention is to cross-compile, this is not an error.")
-
-print("torch.__version__  = ", torch.__version__)
-TORCH_MAJOR = int(torch.__version__.split('.')[0])
-TORCH_MINOR = int(torch.__version__.split('.')[1])
-
-if TORCH_MAJOR == 0 and TORCH_MINOR < 4:
-      raise RuntimeError("APEx requires Pytorch 0.4 or newer.\n" +
-                         "The latest stable release can be obtained from https://pytorch.org/")
 
 setup(
     name='apex',
